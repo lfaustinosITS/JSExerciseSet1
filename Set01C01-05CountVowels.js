e312e6c7723853fb7da9b710d183a9034d5beb44
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-07-11 17:06:26 -0600

    Improved code
    The function returns a number. Input can be a string or a number. You can choose whether to count vowels or numbers

--- Set01C01-05CountVowels.js
+++ Set01C01-05CountVowels.js
@@ -1,13 +1,20 @@
-function countVowelsAndDigits(str) {
-  const vowelCount = (str.match(/[aeiou]/gi) || []).length;
-  const digitCount = (str.match(/\d/g) || []).length;
-  return {
-    vowelCount,
-    digitCount
-  };
+function countVowelsAndDigits(input,type = 'v') {
+  let str= input.toString()
+  if (type == 'v'){
+    const vowelCount = (str.match(/[aeiou]/gi) || []).length;
+    return vowelCount;
+  }
+  else if (type == 'n') {
+    const digitCount = (str.match(/\d/g) || []).length;
+    return digitCount;
+  }
+  else{
+    throw new Error("The character type to count is invalid. Use 'n' for number or 'v' for vowel")
+  }
 }
-  
-console.log(countVowelsAndDigits("Hello World")); // { vowelCount: 3, digitCount: 0 }
-console.log(countVowelsAndDigits("OpenAI1234")); // { vowelCount: 4, digitCount: 4 }
-console.log(countVowelsAndDigits("Lorem ipsum dolor sit amet")); // { vowelCount: 9, digitCount: 0 }
+console.log(countVowelsAndDigits(1234));
+console.log(countVowelsAndDigits(1234,'n'));
+console.log(countVowelsAndDigits("Hello World"));
+console.log(countVowelsAndDigits("OpenAI1234",'n'));
+console.log(countVowelsAndDigits("Lorem ipsum dolor sit amet",'v'));
   
